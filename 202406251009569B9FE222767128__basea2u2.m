07fe6e8ea0636006e8979a891dc0d848eb7822aa
CAuOH <carlosau.nunez@pregrado.uoh.cl>
2024-06-25 10:30:06 -0400

    Add files via upload

---
+++ 202406251009569B9FE222767128__basea2u2.m
@@ -0,0 +1,31 @@
+%% Comentar el funcionamiento del progragrama
+%Funcionamiento, inputs y outputs:
+%Autores:
+%Fecha:
+
+%% Definir variables
+
+% N° de repeticion
+n=[1 2 3 4 5 6 7 8 9 10];
+
+% Distancia [cm]
+d=[30 40 50 60 70 80 90 100 110 120];
+
+% Tiempo [s]
+t=[0.3 0.41 0.5 0.56 0.69 0.72 0.82 1.04 1.19 1.22];
+
+
+
+%% 2. Utilizando ciclos, calcular a y v}
+
+%% 3. Segun lo seleccionado por el usuario:
+
+% a) Mostrar el vector seleccionado.
+
+% b) Mostrar Grafico velocidad vs tiempo o Grafico aceleracion vs tiempo, segun lo seleccionado.
+
+% c) Mostrar Graficos en formato subplot.
+
+%% 1. Solicitar al usuario ingresar en minuscula a o v
+
+disp(a)

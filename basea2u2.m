f7705c99f90dc3229943f0313d0ab686c0cd2fca
CAuOH <carlosau.nunez@pregrado.uoh.cl>
2024-06-25 11:16:00 -0400

    add plots

--- basea2u2.m
+++ basea2u2.m
@@ -4,6 +4,7 @@
 %Fecha: 25-06-2024
 clear
 clc
+close all
 %% Definir variables
 
 % N° de repeticion
@@ -15,9 +16,13 @@ d=[30 40 50 60 70 80 90 100 110 120];
 % Tiempo [s]
 t=[0.3 0.41 0.5 0.56 0.69 0.72 0.82 1.04 1.19 1.22];
 
-
 %% 2. Utilizando ciclos, calcular a y v:
-
+a = (1:1:10);
+v = (1:1:10);
+for i = 1:1:10;
+    a(i) = (2*a(i))/t(i)^2;
+    v(i) = a(i)*t(i);
+end
 
 %% 3. Segun lo seleccionado por el usuario:
 
@@ -31,11 +36,27 @@ t=[0.3 0.41 0.5 0.56 0.69 0.72 0.82 1.04 1.19 1.22];
 
 answer = input("¿Aceleración o velocidad (a o v)?", "s");
 if answer == "a"
-    %disp(a);
-    %plot(t, a);
+    disp(a);
+    % Creación de gráfico
+    figure(1) %Declara nueva figura
+    plot(t, a)
+    % Etiquetas
+    title("Tiempo vs. aceleración")
+    xlabel("tiempo") 
+    ylabel("aceleración")
+    grid
+
 elseif answer == "v"
-    %disp(v);
-    %plot(t, a);
+    disp(v);
+    % Creación de gráfico
+    figure(2) %Declara nueva figura
+    plot(t, v)
+    % Etiquetas
+    title("Tiempo vs. velocidad")
+    xlabel("tiempo") 
+    ylabel("velocidad")
+    grid
 else
-    disp('Solo se admite "a" o "v".')
+    disp('Solo se admite la letra "a" o la letra "v". Reinicie el programa.')
 end
+
